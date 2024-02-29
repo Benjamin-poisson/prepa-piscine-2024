89604eaccc60ee9cf0ce8a517cbc9d74f8d5f754
Benjamin <poisson.benjamin69@gmail.com>
2024-02-29 19:24:11 +0100

    C02 69\100 passed

--- c02/ex06/ft_str_is_printable.c
+++ c02/ex06/ft_str_is_printable.c
@@ -1,52 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*   ft_str_is_printable.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bpoisson <bpoisson@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/27 17:18:57 by bpoisson          #+#    #+#             */
-/*   Updated: 2024/02/29 17:02:35 by bpoisson         ###   ########.fr       */
+/*   Created: 2024/02/29 18:46:24 by bpoisson          #+#    #+#             */
+/*   Updated: 2024/02/29 18:48:35 by bpoisson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-unsigned int	ft_strlen(char *str)
+int	ft_str_is_printable(char *str)
 {
 	int	i;
 
 	i = 0;
 	while (str[i] != '\0')
 	{
+		if (str[i] < ' ' || str[i] > '~')
+		{
+			return (0);
+		}
 		i++;
 	}
-	return (i);
-}
-
-char	*ft_strncpy(char *dest, char *src, unsigned int n)
-{
-	unsigned int	i;
-
-	i = 0;
-	if (n <= ft_strlen(src))
-	{
-		while (i < n)
-		{
-			dest[i] = src[i];
-			i++;
-		}
-	}
-	else if (n > ft_strlen(src))
-	{
-		while (src[i] != '\0')
-		{
-			dest[i] = src[i];
-			i++;
-		}
-		while (i < ft_strlen(dest))
-		{
-			dest[i] = 0;
-			i++;
-		}
-	}
-	return (dest);
+	return (1);
 }
--- c02/ex08/ft_strlowcase.c
+++ c02/ex08/ft_strlowcase.c
@@ -1,52 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*   ft_strlowcase.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bpoisson <bpoisson@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/27 17:18:57 by bpoisson          #+#    #+#             */
-/*   Updated: 2024/02/29 17:02:35 by bpoisson         ###   ########.fr       */
+/*   Created: 2024/02/29 19:12:49 by bpoisson          #+#    #+#             */
+/*   Updated: 2024/02/29 19:13:36 by bpoisson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-unsigned int	ft_strlen(char *str)
+char	*ft_strlowcase(char *str)
 {
 	int	i;
 
 	i = 0;
 	while (str[i] != '\0')
 	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+		{
+			str[i] = str[i] + 32;
+		}
 		i++;
 	}
-	return (i);
-}
-
-char	*ft_strncpy(char *dest, char *src, unsigned int n)
-{
-	unsigned int	i;
-
-	i = 0;
-	if (n <= ft_strlen(src))
-	{
-		while (i < n)
-		{
-			dest[i] = src[i];
-			i++;
-		}
-	}
-	else if (n > ft_strlen(src))
-	{
-		while (src[i] != '\0')
-		{
-			dest[i] = src[i];
-			i++;
-		}
-		while (i < ft_strlen(dest))
-		{
-			dest[i] = 0;
-			i++;
-		}
-	}
-	return (dest);
+	return (str);
 }
