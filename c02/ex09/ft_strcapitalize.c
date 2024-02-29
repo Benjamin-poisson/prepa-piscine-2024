12050cf9a50a9e955a9f19d416fb2a13b23aa2eb
Benjamin <poisson.benjamin69@gmail.com>
2024-02-29 19:32:25 +0100

    .gitignore update

--- c02/ex09/ft_strcapitalize.c
+++ c02/ex09/ft_strcapitalize.c
@@ -6,8 +6,46 @@
 /*   By: bpoisson <bpoisson@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/29 19:14:33 by bpoisson          #+#    #+#             */
-/*   Updated: 2024/02/29 19:23:05 by bpoisson         ###   ########.fr       */
+/*   Updated: 2024/02/29 19:28:15 by bpoisson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
+#include <stdio.h>
+
+int	ft_char_is_uppercase(char c)
+{
+	if (c < 'A' || c > 'Z')
+	{
+		return (0);
+	}
+	return (1);
+}
+
+int	ft_char_is_lowercase(char c)
+{
+	if (c < 'a' || c > 'z')
+	{
+		return (0);
+	}
+	return (1);
+}
+
+char	*ft_strcapitalize(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if ()
+		{
+			
+		}
+	}
+}
+
+int	main(void)
+{
+
+}
